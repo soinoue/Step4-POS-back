54f140738608f75f7c534d630694f87a73fa4b6e
soinoue <132285362+soinoue@users.noreply.github.com>
2024-04-21 16:33:44 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+#MySQL(Local)
+MYSQL_SERVER="localhost"
+MYSQL_USER="root"
+MYSQL_PASSWORD=""
+MYSQL_DB="pop-make-up_pos-app"
+PRODUCT_TABLE="PRODUCTS"
+Transaction_TABLE="transactions"
+
+#MySQL(Azure)
+MYSQL_SERVER_ONASURE="tech0-db-step4-studentrdb-2.mysql.database.azure.com"
+MYSQL_USER_ONASURE="tech0gen5student"
+MYSQL_PASSWORD_ONASURE="vY7JZNfU2"
+MYSQL_DB_ONASURE="pop-make-up_db"
+MYSQL_SSL_CONFIG = "/Users/yakushijimana/Desktop/soinoue/tech0/Step4/STEP4_pop-make-up-main/DigiCertGlobalRootCA.crt.pem"
