4ea487c6082591ad9e4807d5d7ce4ea70dd66a66
kawananoriyasu <noriyasukawana@outlook.jp>
2024-05-05 22:14:52 +0900

    http⇒https

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ origins = [
     "https://127.0.0.1:8000",
     "https://127.0.0.1:8001",
     "https://127.0.0.1:8001",
-    "http://tech0-gen-5-step4-studentwebapp-3.azurewebsites.net/",
+    "https://tech0-gen-5-step4-studentwebapp-3.azurewebsites.net/",
     "*",
 ]
 
